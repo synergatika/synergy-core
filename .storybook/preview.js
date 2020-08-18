ff81ae07079b9131794d865288336e81bfaac274
George Theofilis <theofilis.g@gmail.com>
2020-08-18 22:44:40 +0300

    Fix issues

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,6 +5,10 @@ import docJson from "../documentation.json";
 
 setCompodocJson(docJson);
 
+import '!style-loader!css-loader!../node_modules/@angular/material/prebuilt-themes/indigo-pink.css';
+import '!style-loader!css-loader!../node_modules/bootstrap/dist/css/bootstrap.css';
+import '!style-loader!css-loader!../node_modules/@mdi/font/css/materialdesignicons.min.css';
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
