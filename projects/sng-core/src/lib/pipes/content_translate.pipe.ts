82f38ea0990a02a08eb48567ed50657dea02ad79
Dimitris Mytakis <dmytakis@gmail.com>
2020-12-21 16:49:52 +0200

    fix rich content style

--- projects/sng-core/src/lib/pipes/content_translate.pipe.ts
+++ projects/sng-core/src/lib/pipes/content_translate.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
-import Content from '../models/content.model';
+import { Content } from '../model';
 
 @Pipe({
   name: 'content_translate',
