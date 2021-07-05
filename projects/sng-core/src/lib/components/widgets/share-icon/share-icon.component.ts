e7e6ad214796d0786a7f3847ccce4f87ab735042
Greg <pastsar@gmail.com>
2021-07-05 20:10:02 +0300

    worked on social share

--- projects/sng-core/src/lib/components/widgets/share-icon/share-icon.component.ts
+++ projects/sng-core/src/lib/components/widgets/share-icon/share-icon.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'sng-share-icon',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./share-icon.component.scss']
 })
 export class ShareIconComponent {
-  constructor() { }
+
+  /**
+   * Imported Variables
+   */
+  @Input() item: any;
+
+  /**
+   * Other ariables
+   */
+  public showNav = false;
+  public shareUrl : string;
+
+  constructor() {
+    this.unsubscribe = new Subject();
+  }
+
+  /**
+   * On Init
+   */
+  ngOnInit(): void {
+    console.log(this.item);
+    this.shareUrl = "https://synergatika.gr/"+this.item.type+"/"+this.item.slug
+  }
+
+  /**
+   * On destroy
+   */
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+    this.loading = false;
+
+  }
+
+  openShare() {
+    if (navigator.share) {
+      navigator.share({
+        title: this.item.title,
+        url:   this.shareUrl,
+      }).then(() => {
+        //Nothing
+      })
+        .catch(console.error);
+    } else {
+      this.showNav = true;
+    }
+  }
+
 }
